5af61a0d3b04456c8925f333b7df2c6eaa76930f
Emma Imber <emma-jo.imber@guardian.co.uk>
2023-12-02 16:21:34 +0000

    Day 2

---
+++ day2/p1.py
@@ -0,0 +1,37 @@
+inputInLines = open('input.txt', 'r').readlines()
+
+possibleGameIdSum = 0
+
+for game in inputInLines:
+    gameId = game.split(': ')[0].split(' ')[1]
+    rounds = game.rstrip().split(': ')[1].split('; ')
+    gameIsPossible = True
+
+    for round in rounds:
+        roundIsPossible = True
+        roundDiceCounts = round.split(', ')
+
+        for diceCount in roundDiceCounts:
+            colour = diceCount.split(' ')[1]
+            match colour:
+                case 'red':
+                    if int(diceCount.split(' ')[0]) > 12:
+                        roundIsPossible = False
+                case 'green':
+                    if int(diceCount.split(' ')[0]) > 13:
+                        roundIsPossible = False
+                case 'blue':
+                    if int(diceCount.split(' ')[0]) > 14:
+                        roundIsPossible = False
+        
+        if roundIsPossible == False:
+            gameIsPossible = False
+    
+    if gameIsPossible:
+        possibleGameIdSum += int(gameId)
+
+print(possibleGameIdSum)
+
+
+
+    
---
+++ day2/p2.py
@@ -0,0 +1,36 @@
+inputInLines = open('input.txt', 'r').readlines()
+
+gamePowerSum = 0
+
+for game in inputInLines:
+    gameId = game.split(': ')[0].split(' ')[1]
+    rounds = game.rstrip().split(': ')[1].split('; ')
+    maxGameValues = {
+        'red': 0,
+        'green': 0,
+        'blue': 0,
+    }
+
+    for round in rounds:
+        roundDiceCounts = round.split(', ')
+
+        for diceCount in roundDiceCounts:
+            colour = diceCount.split(' ')[1]
+            match colour:
+                case 'red':
+                    if int(diceCount.split(' ')[0]) > maxGameValues['red']:
+                        maxGameValues['red'] = int(diceCount.split(' ')[0])
+                case 'green':
+                    if int(diceCount.split(' ')[0]) > maxGameValues['green']:
+                        maxGameValues['green'] = int(diceCount.split(' ')[0])
+                case 'blue':
+                    if int(diceCount.split(' ')[0]) > maxGameValues['blue']:
+                        maxGameValues['blue'] = int(diceCount.split(' ')[0])
+
+    gamePowerSum += maxGameValues['red'] * maxGameValues['green'] * maxGameValues['blue']
+
+print(gamePowerSum)
+
+
+
+    
