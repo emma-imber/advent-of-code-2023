e9be6929aedf4ea89a05e0a1888f80726b76782f
Emma Imber <emma-jo.imber@guardian.co.uk>
2023-12-05 14:47:13 +0000

    Part 1 day 5

---
+++ day5/p1.py
@@ -0,0 +1,45 @@
+import re
+import time
+start_time = time.time()
+
+data = open('input.txt').read()
+sections = data.split("\n\n")
+seeds = []
+
+for section in sections:
+    sectionTitle = section.split(':')[0]
+    maps = section.split(':')[1].replace('\n', ' ')
+    mapValues = re.findall(r'\d+', maps)
+    destinationRangeStart, sourceRangeStart, rangeLength = [], [], []
+
+    if sectionTitle == 'seeds':
+        for value in mapValues:
+            seeds.append(int(value))
+        print(seeds)
+
+    else:
+        for valIndex, value in enumerate(mapValues):
+            if valIndex % 3 == 0:
+                destinationRangeStart.append(int(value))
+            elif (valIndex + 2) % 3 == 0:
+                sourceRangeStart.append(int(value))
+            elif (valIndex + 1) % 3 == 0:
+                rangeLength.append(int(value))
+
+        newSeeds = []
+
+        for seed in seeds:
+            mapped = False
+            for sourceIndex, sourceVal in enumerate(sourceRangeStart):
+                if seed >= sourceVal and seed < sourceVal + rangeLength[sourceIndex]:
+                    newSeeds.append(seed + (destinationRangeStart[sourceIndex] - sourceVal))
+                    mapped = True
+
+            if not mapped:
+                newSeeds.append(seed)
+
+        seeds = newSeeds
+
+smallestLocation = min(seeds)
+print(smallestLocation)
+print("--- %s seconds ---" % (time.time() - start_time))
